a1d3b745d3d21f49b7bd4e3e65776796f6072aa6
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-07-12 11:19:34 -0800

    Adding the interface Controls

--- SimpleForm.java
+++ SimpleForm.java
@@ -1,19 +1,44 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class SimpleForm extends Application
-{
+public class SimpleForm extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
-
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		// 1. Create & configure user interface controls
+		//name label
+		Label helloLabel = new Label("Enter your Name: ");
+		//name textbox
+		TextField nameTextBox = new TextField();
+		//Button
+		Button goButton = new Button();
+		goButton.setText("Click Me!");
+		// 2. Make a layout manager
 		
+		
+		// 3. Add controls to the layout manager
+		
+		// 4. Add layout manager to scene
+		
+		// 5. Add scene to a stage
+		
+		// 6. Show the app
 	}
 
 }
