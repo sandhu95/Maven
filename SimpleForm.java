dd53e02d3f1c432395fdfa27a482c40f784a47eb
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-07-12 13:14:33 -0800

    making button work

--- SimpleForm.java
+++ SimpleForm.java
@@ -22,31 +22,43 @@ public class SimpleForm extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// 1. Create & configure user interface controls
-		//name label
+		// name label
 		Label helloLabel = new Label("Enter your Name: ");
-		//name textbox
+		// name textbox
 		TextField nameTextBox = new TextField();
-		//Button
+		// Button
 		Button goButton = new Button();
 		goButton.setText("Click Me!");
+
+		// Add a button Click Handler
+		// Button Click handler = the code you want to run
+		// when person clicks on the button
 		// 2. Make a layout manager
+		goButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				// Logic for what should happen when you push button
+					System.out.println("You Clicked Button!");
+			}
+		});
 		VBox root = new VBox();
-		
+		root.setSpacing(10);
+
 		// 3. Add controls to the layout manager
 		root.getChildren().add(helloLabel);
 		root.getChildren().add(nameTextBox);
 		root.getChildren().add(goButton);
-		
+
 		// 4. Add layout manager to scene
-		
+
 		// 5. Add scene to a stage
-		
-		primaryStage.setScene(new Scene(root,300,250));
+		// both 4&5th step
+		primaryStage.setScene(new Scene(root, 300, 250));
 		primaryStage.setTitle("Example 01");
-		
+
 		// 6. Show the app
 		primaryStage.show();
-		
+
 	}
 
 }
